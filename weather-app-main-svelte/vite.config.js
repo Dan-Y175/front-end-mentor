f5f455e477f406d2e953f0585e8a8daf5de90d96
Daniel <danielyoung509@gmail.com>
2025-10-23 21:35:54 +1300

    update

--- weather-app-main-svelte/vite.config.js
+++ weather-app-main-svelte/vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/front-end-mentor/weather-app-main-svelte-dist',
+  base: '/front-end-mentor/weather-app-main-svelte/dist',
   build: {
   rollupOptions: {
     output: {
