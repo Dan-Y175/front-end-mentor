4c1fcd560f06cff2b70b769f6941e2802fe40579
Daniel <danielyoung509@gmail.com>
2025-10-23 20:53:27 +1300

    testing

--- weather-app-main-svelte/vite.config.js
+++ weather-app-main-svelte/vite.config.js
@@ -4,4 +4,27 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+  rollupOptions: {
+    output: {
+      chunkFileNames: 'assets/js/[name]-[hash].js',
+      entryFileNames: 'assets/js/[name]-[hash].js',
+      
+      assetFileNames: ({name}) => {
+        if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')){
+            return 'assets/images/[name]-[hash][extname]';
+        }
+        
+        if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';   
+        }
+
+        // default value
+        // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+        return 'assets/[name]-[hash][extname]';
+      },
+    },
+  }
+},
+  
 })
